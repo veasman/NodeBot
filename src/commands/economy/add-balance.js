f1b3ad59a30a93f7dc3b5c0cc07743349dddbc6e
veasman <charlton.moren@gmail.com>
2021-03-18 10:50:42 -0500

    Economy system

---
+++ src/commands/economy/add-balance.js
@@ -0,0 +1,31 @@
+const economy = require("../../economy")
+
+module.exports = {
+  commands: ["addbalance", "addbal"],
+  minArgs: 2,
+  minArgs: 2,
+  expectedArgs: "[target] [coins]",
+  permissionError: "You must be a merchant to use this command.",
+  callback: async (message, arguments) => {
+    const mention = message.mentions.users.first();
+
+    if (!mentions) {
+      message.reply("Please tag a user to give coins to");
+      return;
+    }
+
+    const coins = arguments[1];
+    if (isNaN(coins)) {
+      message.reply("Please provide a valid number of coins");
+      return;
+    }
+
+    const guildId = message.guild.id;
+    const userId = mention.id;
+
+    const newCoins = await economy.addCoins(guildId, userId, coins);
+
+    message.reply(`You have give <@${user.id}> ${coins} coin(s). They now have ${newCoins} coin(s)`)
+  },
+  requiredRoles: "𝖒𝖊𝖗𝖈𝖍𝖆𝖓𝖙"
+}
---
+++ src/economy.js
@@ -0,0 +1,74 @@
+const mongo = require("./mongo");
+const profileSchema = require("./schemas/profile-schema");
+
+const coinsCache = {};
+
+module.exports = (client) => {};
+
+module.exports.addCoins = async( guildId, userId, coins) => {
+  const cachedValue = coinsCache[`${guildId}-${userId}`];
+  if (cachedValue) {
+    return cachedValue;
+  }
+
+  return await mongo().then(async (mongoose) => {
+    try{
+      console.log("Running findOneAndUpdate()");
+
+      const result = await profileSchema.findOneAndUpdate({
+        guildId,
+        userId
+      }, {
+        guildId,
+        userId,
+        $inc: {
+          coins
+        }
+      }, {
+        upsert: true,
+        new: true
+      })
+
+      console.log("RESULT:", result);
+
+      coinsCache[`${guildId}-${userId}`] = result.coins;
+
+      return result.coins;
+    } finally {
+      mongoose.connection.close();
+    }
+  })
+}
+
+module.exports.getCoins = async (guildId, userId) => {
+  return await mongo().then(async (mongoose) => {
+    try {
+      console.log("Running findOne()");
+
+      const result = await profileSchema.findOne({
+        guildId,
+        userId
+      });
+
+      console.log("RESULT:", result);
+
+      let coins = 0;
+      if (result) {
+        coins = result.coins;
+      } else {
+        console.log("Inserting a document");
+        await new profileSchema({
+          guildId,
+          userId,
+          coins
+        }).save();
+      }
+
+      coinsCache[`${guildId}-${uesrId}`] = coins;
+
+      return coins;
+    } finally {
+      mongoose.connection.close();
+    }
+  })
+}
